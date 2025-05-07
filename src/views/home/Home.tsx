21bc7988503bf4e6a45e97a3b252e864bec9cc4d
Msedjari <msedjari@elpuig.xeill.net>
2025-05-07 17:43:32 +0200

    feat: Implementar nueva vista de inicio modular y componentes relacionados

---
+++ src/views/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeComponent from '../../components/home/Home';
+
+/**
+ * Componente de vista de inicio
+ * Este componente ahora utiliza el componente modular Home de la carpeta components/home
+ */
+const Home: React.FC = () => {
+  return <HomeComponent />;
+};
+
+export default Home;
