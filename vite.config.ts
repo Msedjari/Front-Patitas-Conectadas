3b7afc3223d87f88155498fcb09ab5938bc8ab39
Fernandodg97 <ferdiaz1997@gmail.com>
2025-05-27 01:55:07 +0200

    fix: Solucion de bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { config } from './src/config'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: config.apiUrl,
+        target: 'http://localhost:4000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
